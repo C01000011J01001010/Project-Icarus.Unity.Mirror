b2346755ced87cbbd541f672bd0d88549dbb0a90
YoonChangJoon <ycjun1209@gmail.com>
2026-07-24 19:32:21 +0900

    코드스맬(잠재적 버그) 발견: Client 인원 변경시 마다 이벤트 발생 필요

--- Assets/Project/Contents/Character/ClientMoveInfoProvider.cs
+++ Assets/Project/Contents/Character/ClientMoveInfoProvider.cs
@@ -31,7 +31,12 @@ namespace Icarus.Character
             {
                 return input;
             }
-
+            Utility.Log("버그 발생! 자세한 내용은 주석 참조!", LogColor.Red);
+            // SharedActor에서 Dictionary로 입력을 보관하기 때문에
+            // 클라이언트id가 앞순서인 유저의 입력이 없는 이상
+            // 나의 클라이언트 id가 유효해도 제대로 된 값을 받아갈 수 없음
+            // 때문에 ServerManager.OnRemoteConnectionState를 사용하여 접속 유저가 변경될때마다 EventBus로 알려서
+            // 접속중인 유저의 숫자가 중요한 객체에 알려줘야함
             return Vector2.zero; // 값이 없으면 (0, 0) 반환
         }
 
